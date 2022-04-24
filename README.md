a4e323ac90083361a0a717cf9b29c3b63c689725
Aaron Glover <aglove2189@gmail.com>
2022-04-23 19:26:31 -0500

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode Blind 75
+
+**Problem** | **Solution** | **Time** | **Space**
+-- | :-: | :-: | :-:
+[TwoSum](https://leetcode.com/problems/two-sum/)
---
+++ templates/twosum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    pass
+
+
+if __name__ == "__main__":
+    assert twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert twoSum([3, 2, 4], 6) == [1, 2]
+    assert twoSum([3, 3], 6) == [0, 1]
